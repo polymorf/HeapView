c158e48b5a3da593b666350ad41682b5c31eeb88
David BÉRARD <contact@davidberard.fr>
2015-11-20 00:01:54 +0100

    add normal write to test1

--- tests/test1.c
+++ tests/test1.c
@@ -1,6 +1,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+void fn_write_loop(char *a,int max, int val) {
+	for(int i=0; i<=max; i++)
+		a[i]=val;
+}
 void fn_overflow_loop(char *a,int max, int val) {
 	for(int i=0; i<=max; i++)
 		a[i]=val;
@@ -15,6 +19,7 @@ int main() {
 	char *b = malloc(0x100-8);
 	char *c = malloc(0x80-8);
 	free(b);
+	fn_write_loop(a, 0x60, 0x60);
 	fn_overflow_loop(a, 0x100-7, 0x81);
 	fn_overflow_single(a,0x100-8,0x81);
 	char *e = malloc(0x100 + 0x80-8);
